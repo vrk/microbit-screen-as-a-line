f27affa3716bbb855ca5f1fc0e291ec50f2af1e4
vrk <vrk@users.noreply.github.com>
2024-08-27 11:28:11 -0400

    Update main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
-})
+namespace screenMagic {
+    export function plotAt(index: number): void {
+        const x = Math.round(index / 5);
+        const y = Math.round(index % 5);
+        led.plot(x, y)
+    }
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,5 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+for (let i = 0; i < 25; ++i) {
+    screenMagic.plotAt(0);
+    basic.pause(100);
+}
