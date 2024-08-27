9494e2aa650d314f71c702193d5dfbbcd119bd46
vrk <vrk@users.noreply.github.com>
2024-08-27 11:32:09 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 namespace screenMagic {
+    /**
+     * Address LEDs linearly column first
+     */
+    //% blockId=screenmagicplotat
+    //% block="plot at index"
+    //% index.min=0 index.max=24
     export function plotAt(index: number): void {
         const x = Math.floor(index / 5);
         const y = index % 5;
